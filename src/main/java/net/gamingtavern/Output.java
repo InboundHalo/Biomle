2abeea1ffe77d302ecf0e24b5c13e412a9446e94
InboundHalo <Kieranbeals@gmail.com>
2023-11-08 00:06:24 -0800

    Added a newln() function

--- src/main/java/net/gamingtavern/Output.java
+++ src/main/java/net/gamingtavern/Output.java
@@ -3,6 +3,10 @@ package net.gamingtavern;
 public class Output {
     private static final Color defaultColor = Color.white;
 
+    public static void newln() {
+        System.out.println();
+    }
+
     public static void println(String line) {
         System.out.println(defaultColor + line);
     }
